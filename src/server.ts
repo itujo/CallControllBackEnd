1365e9bac867695cf070e2a944f58cd5635e3666
Ronaldo Junior <junhiowr@hotmail.com>
2020-11-16 11:59:30 -0300

    refactor

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import express, { Application } from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
-import morgan from 'morgan';
+// import morgan from 'morgan';
 import requireDir from 'require-dir';
 
 const app: Application = express();
@@ -15,7 +15,7 @@ const port: Number = 3333;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 
 mongoose.connect(`mongodb://${ip}:27017/mongo`, {
   useNewUrlParser: true,
